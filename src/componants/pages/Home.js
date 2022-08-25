0981c5eb05f9e01cdeadd5e7e00fa00ae7c51d2e
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-24 21:36:57 -0700

    add content to Home.js

--- src/componants/pages/Home.js
+++ src/componants/pages/Home.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className='containor text-center'>
+      <div className='row'>
+        <div className='col align-self-center'>
+          <h1>Home Page</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col align-self-start'>
+          <a href='https://pbarkley.github.io/Helping-Hand/'> Helping Hand </a><p> a charity referal site </p>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col align-self-center'>
+          <a href='https://james-bus-system.herokuapp.com/'> City Bus System </a><p> a urban bus route builder </p>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col align-self-end'>
+          <a href='https://onlymehere.github.io/weather-travel-app/'> Travel Weather </a><p>find weather data from a city of your choice</p>
+        </div>
+
+      </div>
     </div>
   );
 }
